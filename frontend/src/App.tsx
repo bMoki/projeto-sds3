1d8a0fa56d5f2c7992d4eb1fafa3cf7e103c97fa
bMoki <breno26sonda@gmail.com>
2021-09-11 19:57:12 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import Footer from 'components/footer';
+import NavBar from 'components/navbar';
+import DataTable from 'components/DataTable';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Ola Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <h1 className="text-primary">Ola Mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
+
 }
 
 export default App;
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/bMoki" target="_blank" rel="noreferrer">Breno Sonda</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
